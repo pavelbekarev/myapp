2938b75dcd0a2c87f9c2d3a16f08034b9579f273
pavelbekarev <bekarev.ps@gmail.com>
2026-07-09 10:58:06 +0500

    refactor: add validation rules for ContactForm, install Yup

--- src/features/ContactForm/model/validation.ts
+++ src/features/ContactForm/model/validation.ts
@@ -1,9 +1,22 @@
 import type { ContactFormPayload } from "./types";
+import * as Yup from "yup";
 
 export function validation(formData: ContactFormPayload) {
-  console.debug(formData);
   const validateArray = Object.values(formData).filter((item) => item === "");
-  console.debug(validateArray.length);
 
   return validateArray.length === 0;
 }
+
+export const ContactFormValidation = Yup.object().shape({
+  name: Yup.string()
+    .min(5, "Название должно быть длиннее 5 символов")
+    .required("Поле необходимо заполнить"),
+  phone: Yup.string()
+    .matches(
+      /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/,
+      "Введите корректный номер телефона",
+    )
+    .required("Поле необходимо заполнить"),
+  date: Yup.date().required("Выберите дату заезда"),
+  comment: Yup.string().required("Введите Ваши пожелания"),
+});
